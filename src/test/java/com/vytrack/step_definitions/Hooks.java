c48ef2ab0618323e291b32805bd7e00db130c492
JlanELKO <elkoca.ce@gmail.com>
2022-02-15 21:17:58 +0300

    Login updated with real code

--- src/test/java/com/vytrack/step_definitions/Hooks.java
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -1,18 +1,20 @@
 package com.vytrack.step_definitions;
 
+import com.vytrack.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
 public class Hooks {
     @Before
     public void setUp(){
-        System.out.println("\tthis is coming from BEFORE");
+        System.out.println("\tthis is coming from BEFORE ");
     }
 
     @After
     public void tearDown(){
-        System.out.println("\tthis is coming from After ");
+        Driver.closeDriver();
     }
+
 
     @Before("@db")
     public void setUpdb(){
